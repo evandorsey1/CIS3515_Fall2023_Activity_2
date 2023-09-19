ef2871b77e8c61c99a70e69c23b5dbb5193f1c50
Evan Dorsey <dorseyevan@gmail.com>
2023-09-19 14:51:23 -0400

    Went back and changed IntArray to Array for the populate array step. Also added commented out code on step 3.

--- app/src/main/java/edu/temple/inclassuiacvitivity/MainActivity.kt
+++ app/src/main/java/edu/temple/inclassuiacvitivity/MainActivity.kt
@@ -2,8 +2,10 @@ package edu.temple.inclassuiacvitivity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import android.view.View
 import android.widget.*
+import com.google.android.material.navigation.NavigationBarView.OnItemSelectedListener
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,15 +16,22 @@ class MainActivity : AppCompatActivity() {
         val displayTextView = findViewById<TextView>(R.id.textDisplay)
 
         /* Step 1: Populate this array */
-        val numberArray = IntArray(100){it*1}
+        val numberArray = Array(100){it*1}
 
 
         /* Step 2: Create adapter to display items from array in Spinner */
         spinner.adapter = ArrayAdapter(this, android.R.layout.simple_dropdown_item_1line, numberArray)
 
 
-        // Step 3: Change TextView's text size to the number selected in the Spinner */
-        //spinner.onItemSelectedListener = object: ...
+//        // Step 3: Change TextView's text size to the number selected in the Spinner */
+//        spinner.onItemSelectedListener = object: OnItemSelectedListener{
+//            override fun onItemSelected(p0: AdapterView): Boolean {
+//                TODO("Not yet implemented")
+//            }
+//
+//
+//
+//        }
 
     }
 }
